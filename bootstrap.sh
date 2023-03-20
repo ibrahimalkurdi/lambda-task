ed4584aefef90511ae2622170b2bc721e971dbc4
Ibrahim Al Kurdi <ibrahimalkurdi@gmail.com>
2023-03-20 06:00:19 +0100

    fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,9 +20,9 @@ source .aws-credentails
 source config.sh
 
 # Ensure the Terraform backend S3 Bucket is created
-if [[ $(aws s3api head-bucket --bucket "$TF_S3_BUCKET" 2>/dev/null; echo $?) != 0 ]]; then
-	echo "[INFO] The $TF_S3_BUCKET Bucket is not exists, Let's create it"
-	aws s3api create-bucket --bucket $TF_S3_BUCKET --region us-east-1
+if [[ $(aws s3api head-bucket --bucket "$TF_VAR_state_bucket" 2>/dev/null; echo $?) != 0 ]]; then
+	echo "[INFO] The $TF_VAR_state_bucket Bucket is not exists, Let's create it"
+	aws s3api create-bucket --bucket $TF_VAR_state_bucket --region us-east-1
 	echo "[INFO] It has been created successully!"
 fi
 
